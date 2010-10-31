d8ce7b91a874d3a9d22f2c9fc5ad01eac1c129cd
Sean Camden <sean@seancamden.com>
2010-10-31 15:22:07 -0700

    Added sidebar call to footer.

--- footer.php
+++ footer.php
@@ -1,9 +1,18 @@
 	<div id="footer">
+
+		<div id="colophon">
+
+<?php
+	get_sidebar( 'footer' );
+?>
+
 		&copy;<?php echo date(Y); ?> <?php bloginfo('name'); ?><br />
 		Powered by <a href="http://wordpress.org/">WordPress</a> using <a href="http://www.fireandknowledge.org/blueprint/">Blueprint</a>.
 
 		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
-	</div>
+                </div><!-- #colophon -->
+
+	</div><!-- /footer -->
 
 </div> <!-- /container -->
 
