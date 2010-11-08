33cf4b69aa4bf904c87119315db3c789eb35c064
Sean Camden <sean@seancamden.com>
2010-11-07 23:03:12 -0800

    Sidebar widgets working.

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="column span-3 last" id="sidebar">
 	
 		<ul>
-			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
+<?php /* Widgetized sidebar, if you have the plugin installed. */
 if ( ! get_post_custom_values('hidesidebar1') && is_active_sidebar('primary-widget-area') ): ?>
 
 	<div id="primary-widget-area" class="widgets primary">
@@ -74,7 +74,6 @@ if ( ! get_post_custom_values('hidesidebar1') && is_active_sidebar('primary-widg
 				</li>
 			<?php } ?>
 			
-			<?php endif; ?>
 
 		</ul>
 </div> <!-- /sidebar -->
