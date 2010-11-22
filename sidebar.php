0bd8463b17b3d01139d183014ae68d1b9745d06a
Sean Camden <sean@seancamden.com>
2010-11-22 10:53:56 -0800

    Removing div from around primary widget area.

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,12 @@
 <div class="column span-3 last" id="sidebar">
 	
-		<ul>
+		<ul id="primary-widget-area" class="widgets primary">
 <?php /* Widgetized sidebar, if you have the plugin installed. */
 if ( ! get_post_custom_values('hidesidebar1') && is_active_sidebar('primary-widget-area') ): ?>
 
-	<div id="primary-widget-area" class="widgets primary">
 		<?php dynamic_sidebar('primary-widget-area'); ?>
-	</div><!-- #primary-widget-area -->
 
 <?php endif; ?>
 
-		</ul>
+		</ul><!-- #primary-widget-area -->
 </div> <!-- /sidebar -->
