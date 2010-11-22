419a55ead3681aab41a3fd76f3439189f7f52e9b
Sean Camden <sean@seancamden.com>
2010-11-22 11:06:13 -0800

    Removed unknown plugin reference from the sidebar.

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="column span-3 last" id="sidebar">
 	
 		<ul id="primary-widget-area" class="widgets primary">
-<?php /* Widgetized sidebar, if you have the plugin installed. */
+<?php /* Widgetized sidebar */
 if ( ! get_post_custom_values('hidesidebar1') && is_active_sidebar('primary-widget-area') ): ?>
 
 		<?php dynamic_sidebar('primary-widget-area'); ?>
