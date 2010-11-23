26c0781dfa3352168c08245bf12376dfcf0503b6
Sean Camden <sean@seancamden.com>
2010-11-22 18:33:29 -0800

    Still tweaking the nav menu.

--- header.php
+++ header.php
@@ -40,10 +40,7 @@
   <!-- Navigation -->
   <div class="column span-24 first large" id="nav">
       <div class="content">
-        <ul class="tabs">
-          <li class="<?php if (((is_home()) && !(is_paged())) or (is_archive()) or (is_single()) or (is_paged()) or (is_search())) { ?>current_page_item<?php } else { ?>page_item<?php } ?> first"><a href="<?php echo get_settings('home'); ?>">Home</a></li>
           <?php wp_nav_menu(  array( 'theme_location' => 'header-menu' ));  ?> 
-        </ul>
       </div>
   </div>
   <!-- End Navigation -->
