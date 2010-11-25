bbfad47bdeeb0bffdcc146a7d4a94b1612ab1aba
Sean Camden <sean@seancamden.com>
2010-11-25 13:56:26 -0800

    Added if statement to apply the span-7 class when post is displayed on the home page.

--- post.php
+++ post.php
@@ -1,4 +1,4 @@
-	<div class="post" id="post-<?php the_ID(); ?>">
+	<div class="post <?php if ( is_home() ) { echo "span-7"; }; ?>" id="post-<?php the_ID(); ?>">
 
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		<p class="small">
