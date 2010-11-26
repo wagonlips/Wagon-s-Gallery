5c5f775caa53b9d507ddc63492fbadc09d5645f6
Sean Camden <sean@seancamden.com>
2010-11-25 22:46:14 -0800

    Set a fixed height to the post section that contains the date and categories.

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 	<div class="post <?php if ( is_home() ) { echo "span-7"; }; ?>" id="post-<?php the_ID(); ?>">
 
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php echo substr($post->post_title,0,20); if ((strlen($post->post_title)>20) && is_home()) { echo "..."; } ?></a></h2>
-		<p class="small">
+		<p class="small <?php if ( is_home() ) { echo "home"; } ?>">
 			<?php the_time('F jS, Y') ?> &nbsp;|&nbsp; 
 			<!-- by <?php the_author() ?> -->
 			Published in
